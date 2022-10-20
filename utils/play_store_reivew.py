446f296b9b8cefd2445e90c488345eaa79798eaf
InYoung Park <dlsdudg15@naver.com>
2022-10-20 11:06:53 +0900

    Create play_store_reivew.py

---
+++ utils/play_store_reivew.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+
+from konlpy.tag import Okt
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+
+
+korean_stopwords_path = "./korean_stopwords.txt"
+with open(korean_stopwords_path, encoding="utf-8") as f:
+    ko_stop_words = f.readlines()
+
+ko_stop_words = [x.strip() for x in ko_stop_words]
+okt = Okt()
+
+reg_token = RegexpTokenizer("[\\w]+")
+en_stop_words = stopwords.words("english")
+lemmatizer = WordNetLemmatizer()
+
+
+def draw(country: str, app_name: str) -> None:
+    df = pd.read_csv(f"csvs/play-store/{app_name}_{country}.csv")
+    df["result"] = np.nan
+
+    if country == "kr":
+        for index, review in enumerate(df["content"]):
+            result = []
+            text = okt.normalize(review)
+            morphs = okt.morphs(text)
+
+            for word in morphs:
+                if len(word) > 1 and word not in ko_stop_words:
+                    result.append(word)
+
+            temp = " ".join(result)
+            df.iloc[index, -1] = temp
+    else:
+        for index, review in enumerate(df["content"]):
+            result = []
+
+            if type(review) == str:
+                word_tokens = reg_token.tokenize(review)
+
+                for word in word_tokens:
+                    if len(word) > 1 and word not in en_stop_words:
+                        result.append(lemmatizer.lemmatize(word))
+
+                df.iloc[index, -1] = " ".join(result)
+
+    df["result"].to_csv(
+        f"csvs/play-store/{app_name}_{country}_result.csv", index=False, header=False
+    )
+
+
+country_list = ["kr", "us"]
+app_name_list = ["sleep_town", "forest"]
+
+for i in range(len(app_name_list)):
+    app_name = app_name_list[i]
+
+    for country in country_list:
+        draw(country, app_name)
